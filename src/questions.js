b8c75e52f65da52dd2a3857a78dbc19d807e4ecf
Corentin Bacconnais <corentin.bacconnais@gmail.com>
2021-01-21 18:43:26 +0100

    Fix lint

--- src/questions.js
+++ src/questions.js
@@ -1,14 +1,14 @@
 const questions = require('./public/questions.json')
 
 exports.getQuestions = () => {
-    var hvbaQuestions = getAleatoryQuestions(questions.hvba)
-    var omgQuestions = getAleatoryQuestions(questions.omg)
-    var testQuestions = getAleatoryQuestions(questions.tests)
+  var hvbaQuestions = getAleatoryQuestions(questions.hvba)
+  var omgQuestions = getAleatoryQuestions(questions.omg)
+  var testQuestions = getAleatoryQuestions(questions.tests)
  
-    return {test: testQuestions, hvba: hvbaQuestions, omg: omgQuestions}
+  return {test: testQuestions, hvba: hvbaQuestions, omg: omgQuestions}
 }
 
 function getAleatoryQuestions(questions) {
-    var random = Math.floor(Math.random() * (questions.length))
-    return questions[random]
+  var random = Math.floor(Math.random() * (questions.length))
+  return questions[random]
 }
